c15123efbe070cf671fb6162a10cdaa153b2595b
Anna-Karin <annakarinhamberg@outlook.com>
2022-10-14 11:54:50 +0200

    Two strings, firs and last name, calculator

---
+++ src/main/java/se/lexicon/AnnasCar.java
@@ -0,0 +1,27 @@
+package se.lexicon;
+
+public class AnnasCar {
+    public static void main(String[] args) {
+        System.out.println("Anna-Karin Hambergs Car");
+        System.out.println("Brand: Dacia");
+        System.out.println("Model: Duster");
+        System.out.println("Year :2017");
+        System.out.println("Registration Number: PJG 456");
+        System.out.println("Color: Dark Blue");
+
+
+        String Brand = "Dacia";
+        String Model = "Duster";
+        int Year = 2017;
+        String Registrationnumber = "PJG 456";
+        String Color = "Dark Blue";
+        String sentence = Brand +" "+ Model +" " + Year +" " + Registrationnumber +" " + Color;
+        // sout + Tab
+        System.out.println(sentence);
+    }
+
+
+}
+
+
+
